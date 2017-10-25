8657c4fda67be8fc07d709f653a8048ebf96cb1d
Carlex <packcharce@gmail.com>
2017-10-25 17:52:22 +0200

    Prueba

--- app/src/main/java/com/example/carlex/quiz/Principal.java
+++ app/src/main/java/com/example/carlex/quiz/Principal.java
@@ -20,6 +20,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
+
 public class Principal extends AppCompatActivity {
 
     private final int tiempoFinal = 10000, intervalo = 100;
